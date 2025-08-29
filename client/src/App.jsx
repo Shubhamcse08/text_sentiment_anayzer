4d9c0ec26f567d880c2615368b4718792817d6de
Shubhamcse08 <shubhamcse.eng@gmail.com>
2025-08-29 13:59:01 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   const handleSubmit = async () => {
     if (!text.trim()) return;
-    const res = await axios.post("https://text-sentiment-anayzer-backend.onrender.com", { text });
+    const res = await axios.post("https://text-sentiment-anayzer-backend.onrender.com/api/analyze", { text });
     setResult(res.data);
   };
 
