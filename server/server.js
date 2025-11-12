d0e5e9e98d715150763ed4fe0ec56996403ca014
Shubhamcse08 <shubhamcse.eng@gmail.com>
2025-11-12 14:44:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const path = require("path");
 const app = express();
 const PORT =  5000;
 
-app.use(cors({ origin: ['https://text-sentiment-anayzer.onrender.com', 'http://localhost:3000'] }));
+app.use(cors({ origin: 'https://text-sentiment-anayzer.onrender.com' }));
 app.use(bodyParser.json());
 
 // Route: POST /api/analyze
